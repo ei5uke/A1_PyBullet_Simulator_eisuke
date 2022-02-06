056207d346e336fb2f52176933210e312ee48802
ei5uke <ehirota13@gmail.com>
2022-02-06 13:06:56 -0500

    feat: added main simulation run and exception to catch stopping the simulation midway

--- run_simulation.py
+++ run_simulation.py
@@ -1,7 +1,20 @@
 import gym
 import gym_a1
+from random import random
+import numpy as np
 
 env = gym.make('a1-v0')
 
 #Call env.step(action) in a loop here to take actions
-#Call env.reset() when you feel it's reasonable to reset the gym environment (for example if the robot falls over).
+try:
+    print("Running simulation")
+    for i in range(1000):
+        pos = np.array([1 if random() < 0.5 else -1 for i in range(12) ])
+        action = np.array([random()*2 for i in range(12)])
+        obs, reward, done, info = env.step(pos * action)
+    print("Completed simulation")
+    env.close()
+except:
+    print("Force quit simulation")
+    env.close()
+print("Stopped simulation")
